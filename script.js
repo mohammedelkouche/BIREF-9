d14594bd5bebd865fe79faedcaecfd7066eadb3e
mohammedelkouche <mohammed0elkouche@gmail.com>
2022-01-14 10:09:39 +0100

    récupérer la répence dans le code html

--- script.js
+++ script.js
@@ -39,26 +39,36 @@ const API_KEY = "7a5e19788045ede19291dcbd005e01c2";
 const lat = 33.9911
 const lon = -6.8401
 const API = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+var data ;
 //console.log(API);
 async function getData(){
+
+        // make fetch call (promise call)
     var response = await fetch(API);
-    var data = await response.json();
+
+        // promise data
+    data = await response.json();
     console.log(data);
+
     var info_data = data.daily ;
     console.log(info_data);
+
+    var pushdata = "" ;
     for(var i=0 ; i< info_data.length ; i++){
-        // var A +=`<div class="today" id="current-temp">
-        // <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="weather icon" class="w-icon">
-        // <div class="day">Monday</div>
-        // <div class="day">humiditi : 50</div>
-        // <div class="day">pressure : 1025</div>
-        // <div class="day">wind speed  : 2.56</div>
-        // <div class="day">temperature min : 20</div>
-        // <div class="day">temperature max : 30</div>
-        // <div class="day">weather description : clear sky</div>
+        var dt = new Date(info_data[i].dt*1000);
+        pushdata +=`
+        <div class="today" id="current-temp">
+            <img src="http://openweathermap.org/img/wn/${info_data[i].weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+            <div class="day">${dt.toDateString()}</div>
+            <div class="day">humidity : ${info_data[i].humidity}%</div>
+            <div class="day">pressure : ${info_data[i].pressure}</div>
+            <div class="day">wind speed  : ${info_data[i].wind_speed}</div>
+            <div class="day">temperature min : ${info_data[i].temp.min}&#176;c</div>
+            <div class="day">temperature max : ${info_data[i].temp.max}&#176;c</div>
+            <div class="day">weather description : ${info_data[i].weather[0].description}</div>
     
-        // </div>`
-        // currentWeatherItems.innerHTML = A ;
+        </div>`
+        currentWeatherItems.innerHTML = pushdata ;
     }
 }
 getData()
